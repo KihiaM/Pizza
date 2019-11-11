69f99ac28afc2322ec99c9021598947d999e1884
KihiaM <kihiamuthure@yahoo.com>
2019-11-11 12:33:07 +0300

    add interface js logic

--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,13 @@
+document.getElementsByClassName("toppings").value = 100;
+document.getElementById("delivery").value = 200;
+document.getElementById("stuffed").value = 100;
+document.getElementById("size-s").value = 200;
+document.getElementById("size-m").value = 400;
+document.getElementById("size-l").value = 600;
 
+
+var add = function(pizzaSize, pizzaCrust, pizzaDelivery, pizzaToppings) {
+  return pizzaSize + pizzaCrust + pizzaDelivery + parseInt(pizzaToppings);
+};
 
 
